fa73ed4d34429882161a2633b708755b19afc6fe
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-17 20:49:36 +0530

    Shuffling and distributing cards

---
+++ DeckOfCards/DeckOfCards/ShuffleDeck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeckOfCards
+{
+    class ShuffleDeck:InitialDeck
+    {
+        public static void ShufflingDeck()
+        {
+            //Shuffling deck based on Random method
+            int i;
+            Random random = new Random();
+            for (i = 0; i < totalCards; i++)
+            {
+                int temp = random.Next(0, totalCards);
+                deck = SwapCharacters(deck, temp, i);
+
+            }
+
+        }
+        static string[] SwapCharacters(string[] array, int position1, int position2)
+        {
+            //Swap two index in array
+            string temp = array[position1];
+            array[position1] = array[position2];
+            array[position2] = temp;
+            return (array);
+        }
+    }
+}
